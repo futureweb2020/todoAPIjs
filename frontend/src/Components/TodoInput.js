ba51202e1d83ecef5db2ac56a15baa7504826821
boyboygirl <webking777@hotmail.com>
2019-06-21 20:52:05 -0700

    second

---
+++ frontend/src/Components/TodoInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TodoInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: ''
+        }
+    }
+}
+
+export default TodoInput
